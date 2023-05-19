9a118a3b75bd8041ee8a720b052257e932cd12ab
Foday Malike Kabah <fodaykabah1997@gmail.com>
2023-05-19 15:56:39 +0000

    Major update

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# rock-paper-scissors_game
-Rock, Paper or Scissors game using HTML, CSS &amp; JavaScript
+# Rock Paper Scissors Game 🪨📄✂️
+
+Welcome to the Rock Paper Scissors Game! This is a simple web-based game where you can play the classic Rock Paper Scissors against the computer.
+
+
+## How to Play 🎮
+
+1. Visit the [live demo](https://example.com) of the game.
+2. Click on your preferred choice (Rock, Paper, or Scissors) to make your move.
+3. The computer will randomly choose its move.
+4. The game will determine the winner based on the traditional Rock Paper Scissors rules:
+   - 🪨 Rock beats ✂️ Scissors
+   - 📄 Paper beats 🪨 Rock
+   - ✂️ Scissors beats 📄 Paper
+   - If both players choose the same move, it's a tie.
+5. The score will be updated accordingly, showing your wins, losses, and ties.
+6. To reset the game, click on the Reset button.
+
+## Features ✨
+
+- Simple and intuitive user interface.
+- Play against the computer in the classic Rock Paper Scissors game.
+- Real-time score tracking.
+- Fully responsive design, ensuring a seamless experience across different devices.
+
+## Technologies Used 🛠️
+
+- HTML
+- CSS
+- JavaScript
+
+## Installation ⚙️
+
+1. Clone the repository: `git clone https://github.com/231AG/rock-paper-scissors.git`
+2. Open the `index.html` file in your preferred web browser.
+
+## Contributing 🤝
+
+Contributions are welcome! If you have any suggestions or improvements, feel free to submit a pull request.
+
+## License 📝
+
+This project is licensed under the [MIT License](LICENSE).
+
+---
+
+Enjoy playing the Rock Paper Scissors Game! If you have any questions or feedback, please don't hesitate to reach out.
+
+Author: Foday Malike Kabah (231AG)
---
+++ script.js
@@ -0,0 +1,78 @@
+// Rock, Paper & Scissor
+
+let resultDiv = document.querySelector('#result-text')
+let playerScoreDiv = document.querySelector('#player-score')
+let handsDiv = document.querySelector('#hands')
+
+const totalScore = {playerScore: 0, computerScore: 0}
+
+const getComputerChoice = () => {
+  const rpsChoice = ['Rock', 'Paper', 'Scissors']
+  const ran = Math.floor(Math.random() * rpsChoice.length)
+  
+  return rpsChoice[ran]
+}
+
+const getResult = (playerChoice, computerChoice) => {
+  let score;
+
+  if (playerChoice === computerChoice) {
+    score = 0;
+  } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+    score = 1;
+  } else if (playerChoice == 'Paper' && computerChoice == 'Rock'){
+    score = 1;
+  } else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    score = 1;
+  } else {
+    score = -1;
+  }
+  return score;
+}
+
+const showResult = (score, playerChoice, computerChoice) => {
+
+
+  if (score == -1) {
+    resultDiv.textContent = 'You Lose 😢'
+  } else if (score == 1) {
+    resultDiv.textContent = 'You Win 🤩'
+  } else {
+    resultDiv.textContent = 'Draw 😞'
+  }
+
+  handsDiv.innerText = `👦${playerChoice} vs 🤖${computerChoice}`
+  playerScoreDiv.textContent = `Your Score: ${totalScore.playerScore}`
+}
+
+const onclickRps = (playerChoice) => {
+  const computerChoice = getComputerChoice()
+  const score = getResult(playerChoice, computerChoice)
+
+  totalScore['playerScore'] += score
+
+  showResult(score, playerChoice, computerChoice)
+}
+
+const playGame = () => {
+  const rpsButton = document.querySelectorAll('.rpsButton')
+
+  rpsButton.forEach(rpsButtons => {
+    rpsButtons.addEventListener('click', () => onclickRps(rpsButtons.value))
+  })
+
+  document.querySelector('#endGameButton').addEventListener('click', () => endGame())
+
+}
+
+const endGame = () => {
+  totalScore['playerScore'] = 0;
+  totalScore['computerScore'] = 0;
+
+    resultDiv.textContent = ''
+    playerScoreDiv.textContent = ''
+    handsDiv.textContent = ''
+
+}
+
+playGame()
